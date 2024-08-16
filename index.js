5563f47d81f98bb2f43beceb2800f43b87b01eb9
Md. Mamun <mkmamun031@gmail.com>
2024-08-16 12:18:38 +0600

    Implemented pagination

--- index.js
+++ index.js
@@ -30,16 +30,19 @@ async function run() {
         const database = client.db("HatBazaar")
         const productCollection = database.collection('products')
 
+        app.get('/count', async (req, res) => {
+            const result = await productCollection.countDocuments()
+            console.log(result);
+            res.send({result})
+        })
+
         app.get('/products', async (req, res) => {
             const page = parseInt(req.query.page)
             const size = parseInt(req.query.size)
             const result = await productCollection.find().skip(page * size).limit(size).toArray()
             return res.send(result)
         })
-        app.get('/count', async (req, res) => {
-            const result = await productCollection.estimatedDocumentCount()
-            console.log(result);
-        })
+    
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
