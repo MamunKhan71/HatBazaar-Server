24dc7a04566132a83e101167ab81f7cef8e961eb
Md. Mamun <mkmamun031@gmail.com>
2024-08-16 10:07:21 +0600

    Major Improvements

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 const app = express()
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+    ]
+}))
 const port = process.env.PORT || 5000
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q3zjxg2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// const cookieParser = require('cookie-parser')
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q3zjxg2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -17,6 +27,13 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const database = client.db("HatBazaar")
+        const productCollection = database.collection('products')
+
+        app.get('/products', async (req, res) => {
+            const result = await productCollection.find().toArray()
+            return res.send(result)
+        })
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
